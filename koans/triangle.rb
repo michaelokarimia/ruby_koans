27c76b0ad26969f059aa1f11ede2320f6c34454d
Michael Okarimia <git@michaelokarimia.com>
2010-12-20 23:09:36 +0000

    The truths about triangles

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,19 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+	if (a < 1 || b < 1 || c < 1)
+		fail TriangleError
+	end
+	if((a+b <= c) || (b+c <= a) || (a+c <= b))
+		fail TriangleError
+	end
+	if a==b && b==c
+		return :equilateral  
+	end 
+	if a==b || b==c || a==c
+		return :isosceles    
+	end
+	:scalene
 end
 
 # Error class used in part 2.  No need to change this code.
